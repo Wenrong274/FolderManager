6580e7d226aa059a4b10e9b1ac05118a971fa0ae
WenRong <hybrid0930@gmail.com>
2020-03-08 02:41:13 +0800

    [add] editor window add node

--- Assets/FolderManager/Editor/FolderWindow.cs
+++ Assets/FolderManager/Editor/FolderWindow.cs
@@ -21,41 +21,62 @@ public class FolderWindow : EditorWindow
     private string folderNode;
     private List<string> nodeList = new List<string>();
 
-    [MenuItem("Folder Manager/Editor Path")]
+    [MenuItem("Folder Manage/AA")]
     static void Init()
     {
-
-        EditorWindow window = GetWindow(typeof(FolderWindow));
+        FolderWindow window = (FolderWindow)EditorWindow.GetWindow(typeof(FolderWindow));
         window.Show();
     }
 
     void OnGUI()
     {
-        var Style = new GUIStyle(GUI.skin.label) { fixedWidth = 140, alignment = TextAnchor.MiddleLeft };
+        GUIStyle TitleStyle = new GUIStyle(GUI.skin.label) { fixedWidth = 140, alignment = TextAnchor.MiddleLeft };
+
         var options = Enum.GetNames(typeof(RootPath));
         rootPathIndxed = EditorGUILayout.Popup("Root Path", rootPathIndxed, options);
         folderPath = GetFolderPath((RootPath)rootPathIndxed);
 
         GUILayout.BeginHorizontal();
-        GUILayout.Label("Folder Path", Style);
-        GUILayout.Label(folderPath, Style);
-        GUILayout.EndHorizontal();
-
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Add Node Folder", Style);
+        GUILayout.Label("Add Nodes", TitleStyle);
         folderNode = GUILayout.TextField(folderNode);
-        nodeList.Add(folderNode);
-        if (GUILayout.Button("-", new GUIStyle(GUI.skin.button) { fixedWidth = 40, alignment = TextAnchor.MiddleCenter }))
-        {
-            Debug.Log("-");
-        }
-
         if (GUILayout.Button("+", new GUIStyle(GUI.skin.button) { fixedWidth = 40, alignment = TextAnchor.MiddleCenter }))
         {
-            Debug.Log("+");
+            if (!string.IsNullOrEmpty(folderNode))
+            {
+                nodeList.Add(folderNode);
+                Debug.Log("+");
+            }
         }
         GUILayout.EndHorizontal();
 
+        if (nodeList.Count != 0)
+        {
+            GUILayout.Label(folderPath, TitleStyle);
+            string hyphen = string.Empty;
+            for (int i = 1; i < nodeList.Count; i++)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Label(hyphen + "└" + nodeList[i]);
+                if (GUILayout.Button("-", new GUIStyle(GUI.skin.button) { fixedWidth = 40, alignment = TextAnchor.MiddleCenter }))
+                {
+                    nodeList.RemoveAt(i);
+                }
+                GUILayout.EndHorizontal();
+                hyphen += " ";
+            }
+
+        }
+        GUILayout.BeginHorizontal();
+
+        GUILayout.Label("Folder Path", TitleStyle);
+        GUILayout.Label(folderPath, TitleStyle);
+        GUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Clean All Node"))
+        {
+            nodeList = new List<string>();
+            Debug.Log("Clean");
+        }
     }
 
     private string GetFolderPath(RootPath arg)
